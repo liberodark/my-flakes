06c3d0786e6023de365785adc6df215645e7a511
liberodark <liberodark@gmail.com>
2025-11-27 16:27:34 +0100

    Move to 25.11

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Temporary disable https://github.com/NixOS/nixpkgs/issues/435015
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
