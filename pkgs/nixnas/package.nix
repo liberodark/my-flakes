42cc8ffe46c8da6f4b27710d48ed39fcecfa91b1
liberodark <liberodark@gmail.com>
2026-03-19 15:21:14 +0100

    Update nixnas to 1.0.9

--- pkgs/nixnas/package.nix
+++ pkgs/nixnas/package.nix
@@ -8,16 +8,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "nixnas";
-  version = "1.0.8";
+  version = "1.0.9";
 
   src = fetchFromGitHub {
     owner = "liberodark";
     repo = "nixnas";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-g6U+401hC/cn0Ya0KbOTSnrhN6DWMdo7/hwNMNYAjeM=";
+    hash = "sha256-uiMPyMplM4I2srEknlAWF+mtLzvWVxKJ1LP8JVB2lOs=";
   };
 
-  cargoHash = "sha256-ZQQvZ2wPdyEBprQctJat7ulmtJyiP8lwKhDVn2lqhDA=";
+  cargoHash = "sha256-LiGOuME4/3MfofKg2rMDAAPw0qPpy8nDqV/WSO0xo8I=";
 
   nativeInstallCheckInputs = [
     versionCheckHook
